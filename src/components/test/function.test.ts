7e7bebe37bf816359910da75fd5e49df0db2671e
bohdanloik <bohdanloik@gmail.com>
2021-06-04 13:04:20 +0300

    - add test for multiple function -

--- src/components/test/function.test.ts
+++ src/components/test/function.test.ts
@@ -1,4 +1,5 @@
-import { sum } from "./function";
+import { mult, sum } from "./function";
+
 
 test('sum should be correct', ()=> {
 
@@ -8,8 +9,26 @@ test('sum should be correct', ()=> {
     const c = 3;
 
     //action
-    const result = sum(a, b);
+    const result1 = sum(a, b);
+    const result2 = sum(b, c);
 
     //expect result
-    expect(result).toBe(3)
+    expect(result1).toBe(3);
+    expect(result2).toBe(5);
+})
+
+test('multiple should be correct', ()=> {
+
+
+    const a = 1;
+    const b = 2;
+    const c = 3;
+
+    //action
+    const result1 = mult(a, b);
+    const result2 = mult(b, c);
+
+    //expect result
+    expect(result1).toBe(2);
+    expect(result2).toBe(6);
 })
