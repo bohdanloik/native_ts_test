de5a600bcdfbcad2e12a32b2879b9f8a43924c64
bohdanloik <bohdanloik@gmail.com>
2021-06-07 20:40:27 +0300

    - add completed tasks test -

--- src/components/test/function_04.test.ts
+++ src/components/test/function_04.test.ts
@@ -14,10 +14,23 @@ const cheapPredicate = (course: CoursesPropsType) => {
 }
 test('courses must be cheaper than 210 $', () => {
 //   let cheapCorses = courses.filter(cheapPredicate);
-    let cheapCorses = courses.filter(courses =>courses.price < 210);
+    let cheapCorses = courses.filter(c =>c.price < 210);
     expect(cheapCorses.length).toBe(2);
     expect(cheapCorses[0].price).toBe(100);
     expect(cheapCorses[0].title).toBe('CSS');
 
 
+})
+
+const tasks = [
+    {id: 1, title: 'Milk', isDone: true},
+    {id: 2, title: 'Bread', isDone: false},
+    {id: 3, title: 'Water', isDone: false},
+    {id: 4, title: 'Sugar', isDone: true}
+]
+
+test('only completed tasks', () => {
+    let completedTasks = tasks.filter(t => t.isDone);
+    expect(completedTasks.length).toBe(2);
+    expect(completedTasks[0].title).toBe('Milk');
 })
