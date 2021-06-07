aac5eb052b8951a0a5b87b191f79a87813bb0698
bohdanloik <bohdanloik@gmail.com>
2021-06-07 21:23:21 +0300

    - add peaple arrary and test -

--- src/components/test/function_04.test.ts
+++ src/components/test/function_04.test.ts
@@ -34,3 +34,29 @@ test('only completed tasks', () => {
     expect(completedTasks.length).toBe(2);
     expect(completedTasks[0].title).toBe('Milk');
 })
+
+
+//MAP
+
+type PeoplePropsType = {
+    name: string
+    age: number
+}
+
+const people: Array<PeoplePropsType> = [
+    {name: 'Bohdan Dub', age: 35},
+    {name: 'Walt New', age: 22},
+    {name: 'Oleg House', age: 40},
+    {name: 'Igor Coach', age: 31},
+]
+
+test('message to new users', () => {
+
+    let message = people.map(p=>`Hello ${p.name.split(' ')[0]}. Nice to meet you!`);
+    expect(message.length).toBe(4);
+    expect(message[0]).toBe('Hello Bohdan. Nice to meet you!')
+    expect(message[3]).toBe('Hello Igor. Nice to meet you!')
+
+
+
+})
